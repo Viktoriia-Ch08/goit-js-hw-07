815ba627278f1b057bf148287112b3c2fefe5ec1
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-07-18 21:49:52 +0300

    added functionalities for gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const createImageItems = galleryItems => {
+  return galleryItems
+    .map(
+      img =>
+        `<li class="gallery__item">
+  <a class="gallery__link" 
+  href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+};
+listEl.innerHTML = createImageItems(galleryItems);
+
+listEl.addEventListener('click', imageItemClickHandler);
+
+function imageItemClickHandler(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  event.preventDefault();
+  displayImagePreview(event.target.dataset.source);
+}
+
+const displayImagePreview = imageUrl => {
+  const instance = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+`);
+  instance.show();
+};

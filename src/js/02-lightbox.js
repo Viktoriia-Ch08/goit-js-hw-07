c6514009d110c0e7592cd8dcca47af20e486652a
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-07-19 17:54:58 +0300

    added simpleLightBox library

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createImageItems = galleryItems => {
+  return galleryItems
+    .map(
+      img =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}"/>
+   </a>
+</li>`
+    )
+    .join('');
+};
+listEl.innerHTML = createImageItems(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
